8ad624bd521de758b029efae1373834d4500291b
zhuzhe <zhe.zhu1@pactera.com>
2018-05-22 09:58:12 +0800

    创建项目

---
+++ src/main/java/com/space/ueditor/UeditorApplication.java
@@ -0,0 +1,12 @@
+package com.space.ueditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeditorApplication.class, args);
+	}
+}
