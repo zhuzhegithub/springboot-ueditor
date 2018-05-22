18f10809359f7047221402250148f912c724246d
zhuzhe <zhe.zhu1@pactera.com>
2018-05-22 10:18:39 +0800

    添加ueditor

---
+++ src/main/java/com/space/ueditor/baidu/ueditor/define/ActionState.java
@@ -0,0 +1,5 @@
+package com.space.ueditor.baidu.ueditor.define;
+
+public enum ActionState {
+	UNKNOW_ERROR
+}
---
+++ src/main/java/com/space/ueditor/controller/SaveController.java
@@ -0,0 +1,23 @@
+package com.space.ueditor.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuzhe
+ * @date 2018/5/22 9:59
+ * @email 1529949535@qq.com
+ */
+@Slf4j
+@RequestMapping("/test")
+@RestController
+public class SaveController {
+
+    @RequestMapping("/save")
+    public String save(@RequestParam("content") String content){
+        log.info(content);
+        return content;
+    }
+}
