38dbec712a320465613930e844c7c926d1d103aa
zhuzhe <zhe.zhu1@pactera.com>
2018-05-28 12:01:19 +0800

    添加保存到数据库

--- src/main/java/com/space/ueditor/bo/UEditorBO.java
+++ src/main/java/com/space/ueditor/bo/UEditorBO.java
@@ -1,9 +1,19 @@
 package com.space.ueditor.bo;
 
+import lombok.Data;
+
 /**
  * @author zhuzhe
  * @date 2018/5/28 11:34
  * @email 1529949535@qq.com
  */
+@Data
 public class UEditorBO {
+
+    private Long id;
+    private String name;
+    private String type;
+    private String content;
+    private String createDate;
+    private String updateDate;
 }
--- src/main/java/com/space/ueditor/controller/SaveController.java
+++ src/main/java/com/space/ueditor/controller/SaveController.java
@@ -1,9 +1,14 @@
 package com.space.ueditor.controller;
 
+import com.space.ueditor.bo.UEditorBO;
+import com.space.ueditor.service.UEditorService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author zhuzhe
@@ -14,9 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SaveController {
 
+    private String getDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String format = dateFormat.format(new Date());
+        return format;
+    }
+
+    @Autowired
+    private UEditorService uEditorService;
+
     @RequestMapping("/save")
-    public String save(@RequestParam("content") String content){
-        log.info(content);
-        return content;
+    public Long save(UEditorBO uEditorBO){
+        uEditorBO.setCreateDate(getDate());
+        uEditorBO.setUpdateDate(getDate());
+        return uEditorService.save(uEditorBO);
     }
 }
